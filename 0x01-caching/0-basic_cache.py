2f25bd22e2a79ac39027106f85afd1df1d260745
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-01-30 18:54:17 +0200

    Add docs to implemented functions

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -5,15 +5,17 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """ BasicCache defines:
+    """BasicCache defines:
       - constants of your caching system
       - where your data are stored (in a dictionary)
     """
 
     def put(self, key, item):
+        """Add an item in the cache"""
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """Get an item by key"""
         return self.cache_data.get(key)
