deee40bfc8bdaa2478d92d1d7d1c2f2576bd3dc6
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-06 11:54:33 +0200

    Write 2-app.py

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Basic Flask app"""
+
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+class Config:
+    """Configuration class for Babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+def get_locale():
+    """determine the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config.from_object(Config)
+
+
+babel = Babel(app, locale_selector=get_locale)
+
+
+@app.route("/")
+def welcome():
+    """
+    Outputs “Welcome to Holberton” as page title (<title>)
+    and “Hello world” as header (<h1>)
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
