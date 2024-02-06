033662f7f04e267c6d28288b75327952c17b3ecc
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2024-02-06 13:33:49 +0200

    Remove extra import

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Basic Flask app"""
 
-from flask_babel import Babel, _
+from flask_babel import Babel
 from flask import Flask, render_template, request
 
 
@@ -27,7 +27,7 @@ def get_locale():
 
 
 @app.route("/")
-def welcome():
+def index():
     """
     Outputs “Welcome to Holberton” as page title (<title>)
     and “Hello world” as header (<h1>)
